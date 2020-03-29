464bce2212ee0cb16a9fbd4a02399b4a3c0f5cb3
Haukur Hlöðversson <haukurhlo18@ru.is>
2020-03-29 19:42:02 +0000

    Added redux and routes to app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Login from './components/Login';
 import IRC from './components/IRC';
+import store from './store';
 import './styles.css';
 
 ReactDOM.render(
-    <IRC/>,
+    <Provider store={store}>
+        <Router>
+            <Route exact path={'/'} component={Login} />
+            <Route exact path={'/irc'} component={IRC} />
+        </Router>
+    </Provider>,
     document.getElementById('app')
 );
